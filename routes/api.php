11faeb7d9f3ffef37d53c902a97e44a45e4e7b35
herry88 <herry081288@gmail.com>
2021-06-20 12:04:43 +0700

    DoneAPI

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('get-category',App\Http\Controllers\API\CategoryController::class);
